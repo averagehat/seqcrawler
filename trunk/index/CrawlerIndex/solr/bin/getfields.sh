e46d99d8cc84e1e355663416d4c6a15674042aad
osallou <osallou@5029385b-b014-4093-8000-311bb1a0bede>
2011-06-06 10:01:23 +0000

    update to get version of index jar from env

--- trunk/index/CrawlerIndex/solr/bin/getfields.sh
+++ trunk/index/CrawlerIndex/solr/bin/getfields.sh
@@ -7,7 +7,6 @@ echo "List fields operation"
 echo "Usage: listfields.sh -file outputfilename"
 echo ""
 
-java -cp $INDEXHOME/CrawlerIndex-0.1-jar-with-dependencies.jar  org.irisa.genoue
-st.seqcrawler.index.utils.Utils -listfields $@
+java -cp $INDEXHOME/CrawlerIndex-$INDEXVERSION-jar-with-dependencies.jar  org.irisa.genouest.seqcrawler.index.utils.Utils -listfields $@
 
 echo "Over"
--- trunk/index/CrawlerIndex/solr/bin/merge.sh
+++ trunk/index/CrawlerIndex/solr/bin/merge.sh
@@ -6,7 +6,7 @@ BASEDIR=$(dirname $0)
 echo "Merging operation"
 
 #java -Xmx3000M -cp $INDEXHOME/lib/lucene-core-2.9.1.jar:$INDEXHOME/lib/lucene-misc-2.9.1.jar org/apache/lucene/misc/IndexMergeTool $@
-java -Xmx3000M -cp $INDEXHOME/CrawlerIndex-0.1-jar-with-dependencies.jar org/irisa/genouest/seqcrawler/index/Merge $@
+java -Xmx3000M -cp $INDEXHOME/CrawlerIndex-$INDEXVERSION-jar-with-dependencies.jar org/irisa/genouest/seqcrawler/index/Merge $@
 
 
 
