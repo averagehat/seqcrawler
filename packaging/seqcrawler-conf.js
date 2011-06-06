f11d095157e966bc531be458381ad8cf0e9f9b7b
osallou <osallou@5029385b-b014-4093-8000-311bb1a0bede>
2011-06-06 08:59:43 +0000

    bad import

--- packaging/seqcrawler-conf.js
+++
@@ -1,8 +0,0 @@
-   $pageCountResults = 20;
-   $solrUrl = "http://localhost/solr/select?rows="+$pageCountResults+"&qt=shard&wt=json&";
-   $solrFacet ="facet=on&facet.field=bank&";
-   // MONGO:
-   $storageurl = "http://localhost/mongo";
-   $gburl = "http://localhost";
-   $gbbank = "seqcrawler";
-
--- packaging/seqcrawler/usr/share/seqcrawler/bin/seqcrawler-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Regenerate yeast example
-echo "Generating yeast example, this can take a few minutes..."
-
-/usr/share/seqcrawler/solr/apache-solr-1.4.1/seqcrawler/solr/bin/crawler.sh -f /var/lib/seqcrawler/example/yeast.gff -sh /usr/share/seqcrawler/solr/apache-solr-1.4.1/seqcrawler/solr -sd /index/data -storage mongodb -store -stHost 127.0.0.1 > /var/log/seqcrawler/yeast_index.log
-
-echo "Launch basic tests"
-perl /usr/share/seqcrawler/bin/seqcrawler-test.pl
