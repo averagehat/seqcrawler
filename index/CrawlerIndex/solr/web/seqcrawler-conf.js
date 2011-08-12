5a3a8f2748c43d80e90e16639c0d787b5a6f56e5
osallou <osallou@5029385b-b014-4093-8000-311bb1a0bede>
2011-08-12 10:48:18 +0000

    Use localhost and "anonymize" default bank, set to seqcrawler

--- index/CrawlerIndex/solr/web/seqcrawler-conf.js
+++ index/CrawlerIndex/solr/web/seqcrawler-conf.js
@@ -1,13 +1,13 @@
 // Configuration for seqcrawler
 
    $pageCountResults = 20;
-   $solrUrl = "http://seqcrawler.genouest.org/solr/select?rows="+$pageCountResults+"&qt=shard&wt=json&";
+   $solrUrl = "http://localhost/solr/select?rows="+$pageCountResults+"&qt=shard&wt=json&";
    $solrFacet ="facet=on&facet.field=bank&";
-   // RIAK: $storageurl = "http://seqcrawler.genouest.org/riak/web";
+   // RIAK: $storageurl = "http://localhost/riak/web";
    // MONGO:
-   $storageurl = "http://seqcrawler.genouest.org/mongo";
-   $gburl = "http://seqcrawler.genouest.org";
-   $gbbank = "genouest";
+   $storageurl = "http://localhost/mongo";
+   $gburl = "http://localhost";
+   $gbbank = "seqcrawler";
    
    // Set this variable to debug web interface. Query will send back dummy GFF data with no server query.
    $debugweb = 0;
