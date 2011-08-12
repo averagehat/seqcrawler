899976c99bb0e80c67f711ee7294d088d73cbdb4
osallou <osallou@5029385b-b014-4093-8000-311bb1a0bede>
2011-08-12 10:43:42 +0000

    add missing fields

--- packaging/debian/seqcrawler-conf.js
+++ packaging/debian/seqcrawler-conf.js
@@ -6,3 +6,8 @@
    $gburl = "http://localhost";
    $gbbank = "seqcrawler";
 
+   // Set this variable to debug web interface. Query will send back dummy GFF data with no server query.
+   $debugweb = 0;
+   
+   // Rewrite fields
+   $links = [];
