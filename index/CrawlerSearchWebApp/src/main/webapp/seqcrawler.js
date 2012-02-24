58190f5eb6474b630fae5d4079fabbe3288a0392
osallou <osallou@5029385b-b014-4093-8000-311bb1a0bede>
2012-02-24 08:58:30 +0000

    Add variable declaration for IE compatibility

--- index/CrawlerSearchWebApp/src/main/webapp/seqcrawler.js
+++ index/CrawlerSearchWebApp/src/main/webapp/seqcrawler.js
@@ -9,7 +9,7 @@ $GFFFields = [ 'id' , 'chr' , 'feature', 'start', 'end' ];
 $EMBLFields = [ ];
 
 function testShowGffDoc() {
-	data = '{"responseHeader":{"status":0,"QTime":30,"params":{"facet":"on","start":"0","q":"yeast","facet.field":"bank","qt":"shard","wt":"json"}},"response":{"numFound":16406,"start":0,"docs":[{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=FLO9","strand":".","id":"FLO9","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"ebdf15f9-c3d8-42ff-b959-d22b3f68ec1f","start":[24001],"end":[27969]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=CLN3;oneattr=sampleattr;anotherattr=anyvalue;test1=test;test2=newtest","strand":".","id":"CLN3","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"c1ddd825-5618-418b-bab8-69a8d802bdf9","start":[65779],"end":[67521]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=MAK16","strand":".","id":"MAK16","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"9e2cf512-8253-4cd7-a92f-d0b04f073fed","start":[100226],"end":[101146]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=CYS3","strand":".","id":"CYS3","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"5954d8ed-d19c-4815-a99e-8e9a4d40fd88","start":[130802],"end":[131986]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=ADE1","strand":".","id":"ADE1","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"7cba9dcd-2b2d-43be-853d-0339697af694","start":[169370],"end":[170290]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=PHO11","strand":".","id":"PHO11","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"166f32b1-85b4-49d4-a614-26f8b0fb0ba1","start":[225451],"end":[226854]},{"bank":"yeast","chr":"chrII","feature":"region","attributes":"ID=ILS1","strand":".","id":"ILS1","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"1d63704f-2c42-47cb-964c-e7650f7289f8","start":[81041],"end":[84259]},{"bank":"yeast","chr":"chrII","feature":"region","attributes":"ID=MCM2","strand":".","id":"MCM2","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"db31a7e0-eec4-4a18-9c19-864fb8e2ea96","start":[174923],"end":[177529]},{"bank":"yeast","chr":"chrII","feature":"region","attributes":"ID=RAD16","strand":".","id":"RAD16","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"19b55e74-f3d4-44c3-a177-385606a56987","start":[467242],"end":[469614]},{"bank":"yeast","chr":"chrII","feature":"region","attributes":"ID=SUP45","strand":".","id":"SUP45","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"3cd53a52-6120-4a29-b60c-269889532941","start":[530863],"end":[532176]}]},"facet_counts":{"facet_queries":{},"facet_fields":{"bank":["yeast",16406]},"facet_dates":{}}}';	
+	var data = '{"responseHeader":{"status":0,"QTime":30,"params":{"facet":"on","start":"0","q":"yeast","facet.field":"bank","qt":"shard","wt":"json"}},"response":{"numFound":16406,"start":0,"docs":[{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=FLO9","strand":".","id":"FLO9","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"ebdf15f9-c3d8-42ff-b959-d22b3f68ec1f","start":[24001],"end":[27969]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=CLN3;oneattr=sampleattr;anotherattr=anyvalue;test1=test;test2=newtest","strand":".","id":"CLN3","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"c1ddd825-5618-418b-bab8-69a8d802bdf9","start":[65779],"end":[67521]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=MAK16","strand":".","id":"MAK16","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"9e2cf512-8253-4cd7-a92f-d0b04f073fed","start":[100226],"end":[101146]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=CYS3","strand":".","id":"CYS3","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"5954d8ed-d19c-4815-a99e-8e9a4d40fd88","start":[130802],"end":[131986]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=ADE1","strand":".","id":"ADE1","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"7cba9dcd-2b2d-43be-853d-0339697af694","start":[169370],"end":[170290]},{"bank":"yeast","chr":"chrI","feature":"region","attributes":"ID=PHO11","strand":".","id":"PHO11","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"166f32b1-85b4-49d4-a614-26f8b0fb0ba1","start":[225451],"end":[226854]},{"bank":"yeast","chr":"chrII","feature":"region","attributes":"ID=ILS1","strand":".","id":"ILS1","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"1d63704f-2c42-47cb-964c-e7650f7289f8","start":[81041],"end":[84259]},{"bank":"yeast","chr":"chrII","feature":"region","attributes":"ID=MCM2","strand":".","id":"MCM2","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"db31a7e0-eec4-4a18-9c19-864fb8e2ea96","start":[174923],"end":[177529]},{"bank":"yeast","chr":"chrII","feature":"region","attributes":"ID=RAD16","strand":".","id":"RAD16","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"19b55e74-f3d4-44c3-a177-385606a56987","start":[467242],"end":[469614]},{"bank":"yeast","chr":"chrII","feature":"region","attributes":"ID=SUP45","strand":".","id":"SUP45","stream_content_type":"biosequence/gff","stream_name":"/etc/gbrowse2/solrData/yeast/yeast.gff","seqid":"3cd53a52-6120-4a29-b60c-269889532941","start":[530863],"end":[532176]}]},"facet_counts":{"facet_queries":{},"facet_fields":{"bank":["yeast",16406]},"facet_dates":{}}}';	
 	var obj = jQuery.parseJSON(data);
   	size = obj.response.docs.length;
     for(s=0;s<size;s++) {
@@ -22,7 +22,7 @@ function testShowGffDoc() {
 
  function showGffDoc(doc,subclass) {
 
-	 content='<div id="gff_'+doc['id']+'" class="document'+subclass+'">';
+	 var content='<div id="gff_'+doc['id']+'" class="document'+subclass+'">';
 	 content+='<div class="gff_main_fields">';
 	 for(var field in $GFFFields) {
 		 content+='<div class="field">'+$GFFFields[field]+': '+doc[$GFFFields[field]]+'</div>';
@@ -81,7 +81,7 @@ function testShowGffDoc() {
  }
  
  function showEmblDoc(doc,subclass) {
-	 content='<div id="gff_'+doc['id']+'" class="document'+subclass+'">';
+	 var content='<div id="gff_'+doc['id']+'" class="document'+subclass+'">';
 	 content+='<div class="embl_main_fields">';
 	 for(var key in doc) {
 			if(key.indexOf('stream_')==-1 && key.indexOf('seqid')==-1 && key.indexOf('id')==-1 && key.indexOf('file')==-1) {
@@ -110,7 +110,7 @@ function testShowGffDoc() {
  
  
  function showPdbDoc(doc,subclass) {
-     content='<div id="pdb_'+doc['id']+'" class="document'+subclass+'">';
+     var content='<div id="pdb_'+doc['id']+'" class="document'+subclass+'">';
      content+='<div class="pdb_main_fields">';
      for(var key in doc) {
                     if(key.indexOf('stream_')==-1 && key.indexOf('seqid')==-1 && key.indexOf('file')==-1) {
@@ -128,7 +128,7 @@ function testShowGffDoc() {
 }
  
  function detailsLink(doc) {
-	content='<img  onclick="showDetails(\''+doc["seqid"]+'\',\''+ doc["stream_content_type"] +'\',\''+ doc["stream_name"]  +'\',\''+ doc["file"]  +'\')" title="Show details" alt="Show details" src="images/ihelp.png" class="icon"/>';
+	var content='<img  onclick="showDetails(\''+doc["seqid"]+'\',\''+ doc["stream_content_type"] +'\',\''+ doc["stream_name"]  +'\',\''+ doc["file"]  +'\')" title="Show details" alt="Show details" src="images/ihelp.png" class="icon"/>';
 	return content;
  }
  
